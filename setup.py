9b4b97b4a1ddb1c4d913bc41f67f41f0b97c7fe8
AduchiMergen <aduchimergen@gmail.com>
2013-01-30 19:20:01 +0700

    Openshift Prepare

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='YT2DB',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      author='AduchiMergen',
+      author_email='aduchimergen@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['Flask>=0.7.2'],
+      install_requires=['Flask>=0.9'],
      )
