61eb4babd661cef848b354258e0463c3b5b1d55c
AduchiMergen <aduchimergen@gmail.com>
2013-01-30 20:25:57 +0700

    Move setting in libs dir

--- wsgi/settings_local.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DEBUG = True
-DROPBOX_KEY = 'u5r4elvws36arnb'
-DROPBOX_SECRET = 'xj2o8jgu13wy5ex'
-SECRET_KEY = 'S\xcb\xcd\xf035\xd8\xaf\xa7\xdc\x12-\xa2\xfc5\xe7\x98\x84\x99\xc1\x07\x9f7:'
-JOB_DIR = os.path.join(os.environ['OPENSHIFT_TMP_DIR'],"jobs")
-DOWNLOAD_DIR = os.path.join(os.environ['OPENSHIFT_TMP_DIR'],"video")
